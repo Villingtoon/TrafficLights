d642ae53ecaeccbc739ab4f37f77283fcbbeb8fb
Noob gamer <legofanslv@gmail.com>
2020-06-03 17:09:09 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace TrafficLights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Red = new System.Windows.Forms.PictureBox();
+            this.Yellow = new System.Windows.Forms.PictureBox();
+            this.Green = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Yellow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Red
+            // 
+            this.Red.Location = new System.Drawing.Point(13, 13);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(139, 136);
+            this.Red.TabIndex = 0;
+            this.Red.TabStop = false;
+            // 
+            // Yellow
+            // 
+            this.Yellow.Location = new System.Drawing.Point(14, 157);
+            this.Yellow.Name = "Yellow";
+            this.Yellow.Size = new System.Drawing.Size(139, 136);
+            this.Yellow.TabIndex = 1;
+            this.Yellow.TabStop = false;
+            // 
+            // Green
+            // 
+            this.Green.Location = new System.Drawing.Point(15, 299);
+            this.Green.Name = "Green";
+            this.Green.Size = new System.Drawing.Size(139, 136);
+            this.Green.TabIndex = 2;
+            this.Green.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(166, 450);
+            this.Controls.Add(this.Green);
+            this.Controls.Add(this.Yellow);
+            this.Controls.Add(this.Red);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Yellow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Green)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Red;
+        private System.Windows.Forms.PictureBox Yellow;
+        private System.Windows.Forms.PictureBox Green;
+    }
+}
+
